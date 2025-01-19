b20fa5a9553636d1a2c9fa7438a719dc14356a9b
kingminor <70523148+kingminor@users.noreply.github.com>
2025-01-18 18:38:38 -0700

    Update mission.js

--- mission/scripts/mission.js
+++ mission/scripts/mission.js
@@ -1,9 +1,21 @@
-const themeSelector = document.getElementsById("theme-selector")
+const themeSelector = document.getElementById("theme-selector")
+let current_theme = "light";
+
+const body = document.getElementById("body-tag");
+const logoImage = document.getElementById("logo-image");
+logoImage.src = "imgs/byui-logo_blue.webp";
 
 function changeTheme() {
-    const body = document.getElementById("body-tag");
-    const logoImage = document.getElementById("logo-image");
-    logoImage.src = "imgs/byui-logo_white.png";
+    if (current_theme == "light") {
+        current_theme = "dark";
+        logoImage.src = "imgs/byui-logo_white.png";
+        body.classList.add("dark");
+    }
+    else {
+        current_theme = "light";
+        logoImage.src = "imgs/byui-logo_blue.webp";
+        body.classList.remove("dark");
+    }
 }
 
 themeSelector.addEventListener('change', changeTheme);
