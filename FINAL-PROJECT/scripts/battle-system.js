9d8ee5d2a0ac18dd68b8fcdbffa1dc3f3977976d
kingminor <70523148+kingminor@users.noreply.github.com>
2025-03-27 09:14:09 -0600

    Revert "Update battle-system.js"
    This reverts commit ab688737fb042545e37f36b169beddc6d1604226.

--- FINAL-PROJECT/scripts/battle-system.js
+++ FINAL-PROJECT/scripts/battle-system.js
@@ -640,7 +640,7 @@ attackButton.addEventListener("click", function() {
     actionHolder.style.display = "None"
 });
 
-switchPokemonButton.addEventListener("click", OpenSwitchPokemon(true));
+switchPokemonButton.addEventListener("click", OpenSwitchPokemon);
 
 move1Button.addEventListener("click", function() {
     processTurn("move", yourActivePokemon.move1);
